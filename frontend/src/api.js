64e286d4955e305b20c17dc0263696606cf34b1d
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-06-19 15:21:26 +0530

    url added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://cp-tracker-1.onrender.com/api';
 
 // Analytics API functions
 export const getLeaderboard = async () => {
